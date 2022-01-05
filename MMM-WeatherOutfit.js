2e7149d083786f804ee6e1a5d690c2e59631d647
Magnus Claesson <magnus.claesson@gmail.com>
2022-01-05 21:09:00 +0100

    Added support for translations

--- MMM-WeatherOutfit.js
+++ MMM-WeatherOutfit.js
@@ -9,6 +9,13 @@ Module.register('MMM-WeatherOutfit', {
     return [this.name + '.css'];
   },
 
+  getTranslations: function () {
+    return {
+      en: 'translations/en.json',
+      sv: 'translations/sv.json',
+    };
+  },
+
   notificationReceived: function (notification, payload, sender) {
     if (notification === 'CURRENTWEATHER') {
       this.weather = payload.type.currentWeatherObject;
@@ -22,38 +29,51 @@ Module.register('MMM-WeatherOutfit', {
     let outfit = '';
 
     if (this.hasOwnProperty('weather')) {
-      console.log('CURRENTWEATHER', this.weather);
+      console.log(this.name.toUpperCase(), this.weather);
+      const temp = (this.weather.temperature + this.weather.feelsLikeTemp) / 2;
+      const wind =
+        this.weather.windUnits === 'metric'
+          ? this.weather.useKmh
+            ? this.weather.windSpeed * 0.277778
+            : this.weather.windSpeed
+          : this.weather.windSpeed * 0.44704;
 
-      outfit += 'Ta på dig ';
+      outfit += this.translate('pre') + ' ';
 
-      if (this.weather.temperature > 0) {
-        outfit += 'tjock jacka och vantar';
-      } else if (this.weather.temperature > 10) {
-        outfit += 'jacka med en varm kofta under';
-      } else if (this.weather.temperature > 15) {
-        outfit += 'långbyxor och en varm kofta';
-      } else if (this.weather.temperature > 20) {
-        outfit += 'shorts och t-shirt';
-      } else if (this.weather.temperature > 25) {
-        outfit += 'badbyxor';
+      if (temp >= 2) {
+        outfit += this.translate('2-10');
+      } else if (temp >= 10) {
+        outfit += this.translate('10-15');
+      } else if (temp >= 15) {
+        outfit += this.translate('15-20');
+      } else if (temp >= 20) {
+        outfit += this.translate('20-25');
+      } else if (temp >= 25) {
+        outfit += this.translate('25>');
       } else {
-        outfit += 'vinterjacka, mössa, vantar och halsduk';
+        outfit += this.translate('<2');
       }
 
-      outfit += ' om du ska ut.';
+      outfit += ' ' + this.translate('post');
 
       if (this.weather.rain) {
-        outfit += ' Och glöm inte paraplyet!';
+        if (wind < 5) {
+          outfit += ' ' + this.translate('umbrella');
+        } else {
+          outfit += ' ' + this.translate('windy_umbrella');
+        }
       }
       if (this.weather.snow) {
-        outfit += ' Och glöm inte pulkan!';
+        outfit += ' ' + this.translate('snow');
       }
 
-      div.innerHTML = '<span class="bright">' + outfit + '</span>';
+      div.innerHTML = outfit;
+      div.classList.add('bright');
+
       return div;
     }
 
-    div.innerHTML = '<span>Väntar på vädret...</span>';
+    div.innerHTML = this.translate('waiting');
     div.classList.add('dimmed');
 
     return div;
